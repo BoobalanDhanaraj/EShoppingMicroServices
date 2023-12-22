cebddf60d7398c7f980ff56e602992ae6a54a580
Boobalan D <boobalandhanaraj@gmail.com>
2023-12-22 15:50:06 +0530

    Created Dto files

---
+++ ShoppingCustomerApi/Model/Dto/AddressesDto.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ShoppingCustomerApi.Model.Dto
+{
+    public class AddressesDto
+    {
+        public int AddressID { get; set; }
+        public int CustomerID { get; set; }
+        public string AddressType { get; set; }
+        public string DoorNo { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+    }
+}
